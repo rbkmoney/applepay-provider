33e94c3cf224e0e2ebab2be76889732b675be4b8
Vladimir Pankrashkin <v.pankrashkin@rbkmoney.com>
2018-04-23 14:54:36 +0300

    AP-4: Remove more unnecessary dev methods

--- src/main/java/com/rbkmoney/provider/applepay/service/SessionService.java
+++ src/main/java/com/rbkmoney/provider/applepay/service/SessionService.java
@@ -63,34 +63,6 @@ public class SessionService {
         }
     }
 
-    public String requestSession(String merchantId, String displayName, String domainName, String validationURL) throws CryptoException, CertNotFoundException, APSessionException, WRuntimeException {
-        byte[] identityCert = certStore.getIdentityCert(merchantId);
-        if (identityCert == null) {
-            log.error("Identity cert not found for merchant: {}", merchantId);
-            throw new CertNotFoundException("Not cert for merchant: " + merchantId);
-        }
-
-        try {
-            OkHttpClient client = prepareClient(identityCert, identityPass);
-            log.debug("Http client prepared");
-            Request request = prepareRequest(merchantId, displayName, domainName, validationURL);
-            Response response = client.newCall(request).execute();
-            if (response.isSuccessful()) {
-                return response.body().string();
-            } else {
-                throw new APSessionException("Request session failure", response.body().string());
-            }
-        } catch (IOException e) {
-            WErrorDefinition errDef = errorMapper.mapToDef(e, TraceContext.getCurrentTraceData().getActiveSpan());
-            if (errDef != null) {
-                throw new WRuntimeException(errDef);
-            } else {
-                throw new WRuntimeException(e, new WErrorDefinition());
-            }
-        }
-
-    }
-
     private OkHttpClient prepareClient(byte[] identityCert, char[] pass) throws CryptoException {
         return new OkHttpClient.Builder()
                 .connectTimeout(connTimeoutMs, TimeUnit.MILLISECONDS)
@@ -98,16 +70,6 @@ public class SessionService {
                 .readTimeout(readTimeoutMs, TimeUnit.MILLISECONDS)
                 .addInterceptor(new HttpLoggingInterceptor())
                 .sslSocketFactory(sslProvider.getSSLForClient(identityCert, pass)).build();
-    }
-
-    private Request prepareRequest(String merchantId, String displayName, String domainName, String validationURL) {
-        FormBody requestBody = new FormBody.Builder()
-                .add("merchantIdentifier", merchantId)
-                .add("displayName", displayName)
-                .add("initiative", "web")
-                .add("initiativeContext", domainName)
-                .build();
-        return new Request.Builder().url(prepareUrl(validationURL)).method("POST", requestBody).build();
     }
 
     private Request prepareRequest(String validationURL, String body) {
